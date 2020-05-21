fbda8890a3bdfbff9e3458eb158a0e533175bebe
jmp <jasonpenilla2@me.com>
2020-05-20 17:56:51 -0700

    update checker + load before world

--- src/main/java/fun/ccmc/wt/WanderingTrades.java
+++ src/main/java/fun/ccmc/wt/WanderingTrades.java
@@ -4,6 +4,7 @@ import fun.ccmc.wt.listener.VillagerAcquireTradeEventListener;
 import fun.ccmc.wt.command.CommandWanderingTrades;
 import fun.ccmc.wt.util.Config;
 import fun.ccmc.wt.util.Log;
+import fun.ccmc.wt.util.UpdateChecker;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -28,11 +29,20 @@ public final class WanderingTrades extends JavaPlugin {
         int pluginId = 7597;
         Metrics metrics = new Metrics(this, pluginId);
 
+        new UpdateChecker(this, 79068).getVersion(version -> {
+            if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
+                Log.info("&aYou are running the latest version");
+            } else if(version.contains("SNAPSHOT")) {
+                Log.info("&eYou are running a dev build");
+            } else {
+                Log.info("&eThere is an update available at https://www.spigotmc.org/resources/wanderingtrades.79068/");
+            }
+        });
+
         Log.info("&d[ON]");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
