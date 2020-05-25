5cc75213364f10c4f9cc3a7c7a18ed0654b838c7
jmp <jasonpenilla2@me.com>
2020-05-25 03:19:08 -0700

    option to add trades for player heads of players who have been on the server. configure in playerheads.yml

--- src/main/java/fun/ccmc/wanderingtrades/listener/VillagerAcquireTradeEventListener.java
+++ src/main/java/fun/ccmc/wanderingtrades/listener/VillagerAcquireTradeEventListener.java
@@ -1,13 +1,18 @@
 package fun.ccmc.wanderingtrades.listener;
 
+import com.deanveloper.skullcreator.SkullCreator;
 import fun.ccmc.wanderingtrades.WanderingTrades;
-import fun.ccmc.wanderingtrades.util.TradeConfig;
+import fun.ccmc.wanderingtrades.config.TradeConfig;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.AbstractVillager;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.VillagerAcquireTradeEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.*;
 
@@ -29,6 +34,30 @@ public class VillagerAcquireTradeEventListener implements Listener {
                 plugin.getLog().debug(trader.getLocation().toString());
 
                 ArrayList<MerchantRecipe> newTrades = new ArrayList<>();
+
+                if(plugin.getCfg().getPlayerHeadConfig().isPlayerHeadsFromServer() && randBoolean(plugin.getCfg().getPlayerHeadConfig().getPlayerHeadsFromServerChance())) {
+                    ArrayList<OfflinePlayer> offlinePlayers = new ArrayList<>(Arrays.asList(plugin.getServer().getOfflinePlayers()));
+                    Collections.shuffle(offlinePlayers);
+                    ArrayList<OfflinePlayer> selectedPlayers = new ArrayList<>();
+                    for(int i = 0; i < plugin.getCfg().getPlayerHeadConfig().getPlayerHeadsFromServerAmount(); ++i) {
+                        selectedPlayers.add(offlinePlayers.get(i));
+                    }
+                    selectedPlayers.forEach(player -> {
+                        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+                        SkullCreator.withName(head, player.getName());
+                        ItemMeta meta = head.getItemMeta();
+                        meta.setDisplayName(plugin.getCfg().getPlayerHeadConfig().getName().replace("{PLAYER}", player.getName()));
+                        meta.setLore(plugin.getCfg().getPlayerHeadConfig().getLore());
+                        head.setItemMeta(meta);
+                        head.setAmount(plugin.getCfg().getPlayerHeadConfig().getAmountOfHeadsPerTrade());
+                        MerchantRecipe recipe = new MerchantRecipe(head, 0, plugin.getCfg().getPlayerHeadConfig().getMaxUses(), plugin.getCfg().getPlayerHeadConfig().isExperienceReward());
+                        recipe.addIngredient(plugin.getCfg().getPlayerHeadConfig().getIngredient1());
+                        if(plugin.getCfg().getPlayerHeadConfig().getIngredient2() != null) {
+                            recipe.addIngredient(plugin.getCfg().getPlayerHeadConfig().getIngredient2());
+                        }
+                        newTrades.add(recipe);
+                    });
+                }
 
                 if(plugin.getCfg().isRandomSetPerTrader()) {
                     HashMap<String, TradeConfig> m = plugin.getCfg().getTradeConfigs();
@@ -58,5 +87,9 @@ public class VillagerAcquireTradeEventListener implements Listener {
                 e.setCancelled(true);
             }
         }
+    }
+
+    public static boolean randBoolean(double p) {
+        return Math.random() < p;
     }
 }
