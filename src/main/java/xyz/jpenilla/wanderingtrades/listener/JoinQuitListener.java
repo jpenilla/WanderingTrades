b850733291d62792b64ac637e562954b01a484c4
jmp <jasonpenilla2@me.com>
2020-06-22 15:30:32 -0700

    Remove players who lost permissions on logout

--- src/main/java/xyz/jpenilla/wanderingtrades/listener/JoinQuitListener.java
+++ src/main/java/xyz/jpenilla/wanderingtrades/listener/JoinQuitListener.java
@@ -4,15 +4,22 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import xyz.jpenilla.jmplib.TextUtil;
 import xyz.jpenilla.wanderingtrades.WanderingTrades;
 
 public class JoinQuitListener implements Listener {
 
+    private final WanderingTrades wanderingTrades;
+
+    public JoinQuitListener(WanderingTrades wanderingTrades) {
+        this.wanderingTrades = wanderingTrades;
+    }
+
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        if (WanderingTrades.getInstance().isVaultPermissions()) {
-            if (WanderingTrades.getInstance().getCfg().getPlayerHeadConfig().isPermissionWhitelist()) {
+        if (wanderingTrades.isVaultPermissions()) {
+            if (wanderingTrades.getCfg().getPlayerHeadConfig().isPermissionWhitelist()) {
                 if (e.getPlayer().hasPermission("wanderingtrades.headavailable")) {
                     addHead(e.getPlayer());
                 }
@@ -24,9 +31,18 @@ public class JoinQuitListener implements Listener {
         }
     }
 
+    @EventHandler
+    public void onLeave(PlayerQuitEvent e) {
+        if (wanderingTrades.isVaultPermissions() && wanderingTrades.getCfg().getPlayerHeadConfig().isPermissionWhitelist()) {
+            if (!e.getPlayer().hasPermission("wanderingtrades.headavailable") && wanderingTrades.getStoredPlayers().getPlayers().contains(e.getPlayer().getUniqueId())) {
+                wanderingTrades.getStoredPlayers().getPlayers().remove(e.getPlayer().getUniqueId());
+            }
+        }
+    }
+
     private void addHead(Player player) {
-        if (!WanderingTrades.getInstance().getStoredPlayers().getPlayers().contains(player.getUniqueId()) && !TextUtil.containsCaseInsensitive(player.getName(), WanderingTrades.getInstance().getCfg().getPlayerHeadConfig().getUsernameBlacklist())) {
-            WanderingTrades.getInstance().getStoredPlayers().getPlayers().add(player.getUniqueId());
+        if (!wanderingTrades.getStoredPlayers().getPlayers().contains(player.getUniqueId()) && !TextUtil.containsCaseInsensitive(player.getName(), wanderingTrades.getCfg().getPlayerHeadConfig().getUsernameBlacklist())) {
+            wanderingTrades.getStoredPlayers().getPlayers().add(player.getUniqueId());
         }
     }
 }
