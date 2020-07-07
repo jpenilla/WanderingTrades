a5d1f941ac61f823725d365352968a1d4e574f15
jmp <jasonpenilla2@me.com>
2020-07-06 23:01:30 -0700

    stop named traders from being renamed by nametags

--- src/main/java/xyz/jpenilla/wanderingtrades/listener/ProtectTradersListener.java
+++ src/main/java/xyz/jpenilla/wanderingtrades/listener/ProtectTradersListener.java
@@ -1,11 +1,14 @@
 package xyz.jpenilla.wanderingtrades.listener;
 
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.persistence.PersistentDataType;
 import xyz.jpenilla.wanderingtrades.WanderingTrades;
 
@@ -21,12 +24,23 @@ public class ProtectTradersListener implements Listener {
         EntityType type = e.getEntityType();
         if (type == EntityType.WANDERING_TRADER || type == EntityType.VILLAGER) {
             Entity entity = e.getEntity();
-            NamespacedKey key = new NamespacedKey(plugin, "wtProtect");
-            String i = entity.getPersistentDataContainer().get(key, PersistentDataType.STRING);
+            NamespacedKey wtProtect = new NamespacedKey(plugin, "wtProtect");
+            String i = entity.getPersistentDataContainer().get(wtProtect, PersistentDataType.STRING);
             if (i != null) {
                 if (!e.getDamager().hasPermission("wanderingtrades.damage")) {
                     e.setCancelled(true);
                 }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityInteractEntity(PlayerInteractEntityEvent e) {
+        NamespacedKey wtConfig = new NamespacedKey(plugin, "wtConfig");
+        if (e.getRightClicked().getPersistentDataContainer().has(wtConfig, PersistentDataType.STRING)) {
+            PlayerInventory p = e.getPlayer().getInventory();
+            if ((p.getItemInMainHand().getType() == Material.NAME_TAG) || (p.getItemInOffHand().getType() == Material.NAME_TAG)) {
+                e.setCancelled(true);
             }
         }
     }
