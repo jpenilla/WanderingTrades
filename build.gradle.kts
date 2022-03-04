17b92249fe1738c4da16d9d8782ab8e3c10989b6
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-03-04 10:39:52 -0700

    release: Version 1.7.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "xyz.jpenilla"
-version = "1.7.1-SNAPSHOT".decorateVersion()
+version = "1.7.1".decorateVersion()
 description = "Customizable trades for Wandering Traders."
 
 repositories {
